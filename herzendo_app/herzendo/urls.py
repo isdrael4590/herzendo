ceb9a54c61855e7c12a1dc15f0f709b937831213
José Jácome <josejacomeb@gmail.com>
2026-05-08 18:46:39 -0500

    tarea: añadir el python debug bar

--- herzendo_app/herzendo/urls.py
+++ herzendo_app/herzendo/urls.py
@@ -1,11 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/',  auth_views.LoginView.as_view(),  name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', include('pacientes.urls')),
-    path('referencias/', include('referencias.urls')),
+    path("", include("pacientes.urls")),
+    path("admin/", admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("referencias/", include("referencias.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns += debug_toolbar_urls()

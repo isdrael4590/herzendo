debb63a19dcce5011749bdc41d7dc79cf43aabf0
José Jácome <josejacomeb@gmail.com>
2026-05-08 18:44:28 -0500

    tarea: settings por configuración

---
+++ herzendo_app/herzendo/settings/local.py
@@ -0,0 +1,23 @@
+import os
+
+from .base import *  # noqa: F403
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # noqa: F405
+]
+
+# Python Debug Toolbar
+if DEBUG:
+    import socket
+
+    INSTALLED_APPS.append("debug_toolbar")  # noqa: F405
+    MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")  # noqa: F405
+    # Obtener IP de Docker
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + LOCAL_IPS
+    DEBUG_TOOLBAR_CONFIG = {
+        "SHOW_TOOLBAR_CALLBACK": "debug_toolbar.middleware.show_toolbar_with_docker",
+    }
---
+++ herzendo_app/herzendo/settings/prod.py
@@ -0,0 +1,13 @@
+# No comprobar en ciertas líneas F403 y F405 para compartir las configuraciones del base.py
+from .base import *  # noqa: F403
+
+DEBUG = False
+ADMINS = [("Fernando Jácome", "isdrael4590@gmail.com")]
+STATIC_ROOT = BASE_DIR / "static"  # noqa: F405
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # noqa: F405
+]
+# Configuraciones de seguridad
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
